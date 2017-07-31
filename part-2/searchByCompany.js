1c1c24a73c78eaa9980d2ce321fbe6177f7fa11d
alexandrawaite <alexandrawaite@gmail.com>
2017-07-31 16:38:06 -0700

    finish searchByCompany.js

---
+++ part-2/searchByCompany.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const clientData = fs.readFileSync('./clients.json'.toString())
+const parsedData = JSON.parse(clientData)
+const clientCompany = process.argv[2]
+
+const companyFinder = (company) => {
+  console.log('Finding companies with name "'  + company + '"...')
+  let matchingCompanies = parsedData.filter(obj => obj.company.toLowerCase().startsWith(company.toLowerCase()) == true)
+  let companyResults = matchingCompanies.map(obj => ({
+    "id": obj.id,
+    "company": obj.company,
+    "phone": obj.phone
+  }))
+  console.log(companyResults)
+}
+
+companyFinder(clientCompany)
